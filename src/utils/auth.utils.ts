b3345c995955a97311956df7c1153f3075250bce
asim-momin-7864 <asimmomin.dev@gmail.com>
2026-06-14 15:50:55 +0000

    Implement token generator function

---
+++ src/utils/auth.utils.ts
@@ -0,0 +1,22 @@
+//* auth
+import type { Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { env } from '@config/env.config.js';
+
+// type of sign option
+type SignOptionsExpiresIn = NonNullable<jwt.SignOptions['expiresIn']>;
+
+export const generateTokenAndSetCookie = (userId: string, res: Response): void => {
+  // sign JWT wih user ID
+  const token = jwt.sign({ userId }, env.JWT_SECRET, {
+    expiresIn: env.JWT_EXPIRES_IN as SignOptionsExpiresIn,
+  });
+
+  // attaching with cookie
+  res.cookie('jwt', token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in ms
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: env.NODE_ENV !== 'development', // send cookie only if connection is HTTPS
+  });
+};
