e11602cf0aa078c179605600f6da07a945809ca0
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-17 23:38:42 +0530

    feat: rate limiting for API security

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { register, login, logout } from '#controllers/auth/auth.controller.js';
 import { protectRoute } from '#middlewares/protectRoute.middleware.js';
+import { authLimiter } from '#middlewares/rateLimiter.middleware.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', protectRoute, logout);
+router.post('/register', authLimiter, register);
+router.post('/login', authLimiter, login);
+router.post('/logout', authLimiter, protectRoute, logout);
 
 export default router;
